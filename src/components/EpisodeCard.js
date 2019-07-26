66ac595e4b6cb1e4e3ad543430a8138b8e112448
Landry Irakoze <irakozel@gmail.com>
2019-07-26 12:34:17 -0400

    created tabs, added episode pages

---
+++ src/components/EpisodeCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function CharacterCard (props) {
+//   console.log('props', props);
+//   const details = props;
+//   console.log('details', details);
+
+  return (
+    <div>
+      {/* <img */}
+      {/* <p>{details.name}</p>
+      <img src={details.image} alt={'character'}/> */}
+
+    </div>
+  )
+}
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function LocationCard ({ name, type, dimension, residents }) {
+export default function LocationCard ({ name, type, dimension, residents, image }) {
   // image={image}
-  return (<span>todo: location</span>
+  return (
+    <div>
+      <p>{name}</p>
+      <img src={image} alt={name} />
+      <p>{type}</p>
+      <p>{dimension}</p>
+      <p>{residents}</p>
+    </div>
   )
 }
