69be5ec67e2c5eb897bf4c362f4194b0c3140909
Landry Irakoze <irakozel@gmail.com>
2019-07-26 11:57:02 -0400

    added character cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard (props) {
+  console.log('props', props.character);
+  const details = props.character;
+  console.log('details', details);
+
+  return (
+    <div>
+      {/* <img */}
+      <p>{details.name}</p>
+      <img src={details.image} alt={'character'}/>
+
+    </div>
+  )
 }
